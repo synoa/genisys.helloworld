007afda18aeba44de7fc650299cb8d3b0c46c24e
Tim Pietrusky <timpietrusky@gmail.com>
2018-02-20 14:04:15 +0100

    refactor(logging): Simple logging
    Removed MongoDB, Mongobee and ActiveMQ and only do very simple logging

--- src/main/java/de/synoa/genisys/helloworld/routes/GettingStartedRouteBuilder.java
+++ src/main/java/de/synoa/genisys/helloworld/routes/GettingStartedRouteBuilder.java
@@ -3,8 +3,6 @@ package de.synoa.genisys.helloworld.routes;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-import static de.synoa.genisys.helloworld.configurations.MongoDB.MONGODB_DB_STATS;
-
 @Component
 public class GettingStartedRouteBuilder extends RouteBuilder {
 
@@ -14,12 +12,6 @@ public class GettingStartedRouteBuilder extends RouteBuilder {
 
         from("timer:helloworld?period=5000").routeId("Hello World Route")
             .setBody(constant("Hello Microservice World"))
-            .to("activemq:{{activemq.queue.prefix}}.talk")
-        ;
-
-        from("activemq:{{activemq.queue.prefix}}.talk").routeId("Talk Route")
-            .log("${body}")
-            .to(MONGODB_DB_STATS)
             .log("${body}")
         ;
 
