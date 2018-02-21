fe9e2afa67d16019fa550c232341490706880195
Gerhard Ebelshäuser <g.ebelshaeuser@synoa.de>
2018-02-21 10:37:47 +0100

    Added very simple unit test

---
+++ src/test/java/de/synoa/genisys/helloworld/routes/MyTest.java
@@ -0,0 +1,20 @@
+package de.synoa.genisys.helloworld.routes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MyTest {
+
+    @Test
+    public void goodTest () {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void badTest () {
+        Assert.assertTrue(false);
+    }
+
+
+}
